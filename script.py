5d931b21bfeea8c6563f6ddeb99f3de8c6ade364
abdullah-979 <78228041+abdullah-979@users.noreply.github.com>
2021-01-30 00:19:59 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,47 +1,47 @@
 class script(object):
 
-    START_TEXT = """<b>Hai ,
+    START_TEXT = """<b>Hello Nanba! ,
     
-I'm A simple Zee5 link downloader bot With Permanent Thumbnail Support💯.
+I'm A simple Zee5 Downloader Bot With Permanent Thumbnail Support Added....
 
 Please send me any Zee5 link, I can upload it to telegram as File/Video.
 
-Currently I'm in beta mode 🥺, If found any bugs, report @TroJanzSupport !
+Currently I'm in beta mode 🥺, If found any bugs, report @DoraemonTamil !
 
 Click <i>/help</i> for more details....</b>"""
 
 
-    HELP_USER = """<b>Hai, Follow these steps..</b>
+    HELP_USER = """<b>Hi, Follow My Steps..</b>
  
-1. Send Custom Thumbnail (It will be saved permenantly!)
+1. Send Your Custom Thumbnail (It will be saved permenantly!)
 
-2. Send your zee5 url and select desired option.
+2. Send your zee5 URL and select desired option.
 
 
 NOTE: Download may take some time! So please wait for it to complete!"""
 
 
-    ABOUT_TEXT = """⭕️<b>My Name : TroJanZ Zee5 DL</b>
+    ABOUT_TEXT = """⭕️<b>My Name : Doraemon Zee5 DL </b>
 
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Doraemon_Tamil
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7/a> 
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Zee5-Downloader'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='https://t.me/Doraemon_Tamil'>Not Available Yet</a>"""
 
 
 
     FORMAT_SELECTION = """<b>Choose appropriate option</b> <a href='{}'>⬇️</a>
 
-🎞  - Stream format
+🎞  - Video format
 📁  - File format
 
 <i>NOTE : Taking high resolutions may result in files above 2GB and hence cannot Upload to TG. So better select a medium resolution.</i> 😇
 """    
     
-    UPGRADE_TEXT = "PING at @contactHEXbot"
+    UPGRADE_TEXT = "PING at @AbdullahTN"
     
     DOWNLOAD_START = "Trying to download to my server. This may take a while 😴"
     
@@ -49,17 +49,17 @@ NOTE: Download may take some time! So please wait for it to complete!"""
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
 
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "**Thank you for Using Meh!! ❤️**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "**Thank you for Using Me!! ❤️**"
     
-    SAVED_CUSTOM_THUMB_NAIL = "<b>✅Custom thumbnail Saved.\nThis thumbnail will be Permanent for all future uploads\n\nDo /delthumb to clear your thumbnail!</b>"
+    SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Added Successfully.\nThis thumbnail will be Permanent for all future uploads\n\nDo /delthumb to clear your thumbnail!</b>"
     
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom Thumbnail cleared succesfully."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Deleted Successfully."
     
-    SHOW_THUMB = "@TroJanzHEX\n\nUse /delthumb to clear this thumbnail."
+    SHOW_THUMB = "@Doraemon_Tamil\n\nUse /delthumb to clear this thumbnail."
     
-    NO_THUMB = "SED😕 No saved thumbnails Found!!"
+    NO_THUMB = " No saved thumbnails Found!!"
     
-    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @TroJanzHEX</b>"
+    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @Doraemon_Tamil</b>"
     
     TIMEOUT = "<b><i>Sorry for the delay. It'll help reduce the flood wait</i> 😇\n\nWait for {} sec and try again.</b>"
     
